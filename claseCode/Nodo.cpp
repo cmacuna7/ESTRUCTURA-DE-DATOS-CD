587148374016816ad6b96ca65f57b250b49dbd66
cmbonifaz <mateobvc@hotmail.com>
2024-11-22 09:10:43 -0500

    programa en codeblock

---
+++ claseCode/Nodo.cpp
@@ -0,0 +1,39 @@
+/***************************************************************************************
+ *            UNIVERSIDAD DE LAS FUERZAS ARMADAS ESPE                                  *
+ * Proposito:                      Programa sobre lista simple                         *
+ * Autor:                          Marcelo Acuña, Abner Arboleda, Christian Bonifaz    *
+ * Fecha de creacion:              21/11/2024                                          *
+ * Fecha de modificacion:          21/11/2024                                          *
+ * Materia:                        Estructura de datos                                 *
+ * NRC :                           1992                                                *
+ **************************************************************************************/
+#include "Nodo.h"
+
+using namespace std;
+
+template<typename T>
+Nodo<T>::Nodo(T _dato) {
+    dato = _dato;
+    siguiente = NULL;
+}
+
+template<typename T>
+inline void Nodo<T>::setDato(T _dato) {
+    dato = _dato;
+}
+
+template<typename T>
+T Nodo<T>::getDato() {
+    return dato;
+}
+
+template<typename T>
+void Nodo<T>::setSiguiente(Nodo* _siguiente) {
+    siguiente = _siguiente;
+}
+
+template<typename T>
+Nodo<T>* Nodo<T>::getSiguiente() {
+    return siguiente;
+}
+
