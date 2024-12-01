c846d1c48686250783262e36ff975d0a6d6e1b3b
Abner Arboleda <93734334+adarboleda@users.noreply.github.com>
2024-11-30 19:19:18 -0500

    Proyecto 1.2.3 - Muevo Ayuda a sus archivos respectivos

--- ProyectoPrimerParcial/Menu.cpp
+++ ProyectoPrimerParcial/Menu.cpp
@@ -1,5 +1,6 @@
 #include "Menu.h"
 #include "Backup.cpp"
+#include "Ayuda.cpp"
 #include <iostream>
 #include <conio.h> // Para captura de teclas
 #include <vector>
@@ -7,23 +8,6 @@
 #include <ctime>
 
 using namespace std;
-
-void mostrarAyuda() {
-    system("cls");
-    cout << "=== Ayuda ===\n";
-    cout << "Este programa permite gestionar un registro de libros.\n";
-    cout << "Opciones disponibles:\n";
-    cout << "1. Agregar libro\n";
-    cout << "2. Buscar libro\n";
-    cout << "3. Eliminar libro\n";
-    cout << "4. Ver todos los libros\n";
-    cout << "5. Crear backup\n";
-    cout << "6. Restaurar backup\n";
-    cout << "Use las teclas de flecha para navegar por el menu y Enter para seleccionar.\n";
-    cout << "Presione F1 para mostrar esta ayuda.\n";
-    cout << "\nPresione cualquier tecla para volver al menu principal...\n";
-    _getch();
-}
 
 void mostrarMenu(ListaCircularDoble& lista) {
     vector<string> opciones = {
