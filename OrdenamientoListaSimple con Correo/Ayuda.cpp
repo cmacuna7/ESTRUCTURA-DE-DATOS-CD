f2c94cc5251ad57232ec64d06914ce0ee89f9270
cmbonifaz <mateobvc@hotmail.com>
2024-12-18 07:10:06 -0500

    etiquetas x2

--- OrdenamientoListaSimple con Correo/Ayuda.cpp
+++ OrdenamientoListaSimple con Correo/Ayuda.cpp
@@ -1,3 +1,12 @@
+/*******************************************************************************************************
+ *            UNIVERSIDAD DE LAS FUERZAS ARMADAS ESPE                                                  *
+ * Proposito:                      Ayuda para manejar el sistema                                       *
+ * Autor:                          Marcelo Acuña, Abner Arboleda, Christian Bonifaz                    *
+ * Fecha de creacion:              17/12/2024                                                          *
+ * Fecha de modificacion:          18/12/2024                                                          *
+ * Materia:                        Estructura de datos                                                 *
+ * NRC :                           1992                                                                *
+ *******************************************************************************************************/
 #include "Ayuda.h"
 #include <iostream>
 #include <conio.h>
